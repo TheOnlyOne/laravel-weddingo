f27be9745c333c2755029a2a73477fa77ff2118e
Zohar Yzgeav <zohar.yzgeav@gmail.com>
2016-12-06 23:16:04 +0200

    Initial commit

---
+++ weddingo/resources/views/layouts/master-client-layout/footer-ext.blade.php
@@ -0,0 +1,14 @@
+<!-- jQuery -->
+<script src="{{ URL::asset('plugins/bower_components/jquery/dist/jquery.min.js') }}"></script>
+<!-- Bootstrap Core JavaScript -->
+<script src="{{ URL::asset('plugins/bower_components/bootstrap-rtl-master/dist/js/bootstrap-rtl.min.js') }}"></script>
+<!-- Menu Plugin JavaScript -->
+<script src="{{ URL::asset('plugins/bower_components/sidebar-nav/dist/sidebar-nav.min.js') }}"></script>
+<!--slimscroll JavaScript -->
+<script src="{{ URL::asset('js/jquery.slimscroll.js') }}"></script>
+<!--Wave Effects -->
+<script src="{{ URL::asset('js/waves.js') }}"></script>
+<!-- Custom Theme JavaScript -->
+<script src="{{ URL::asset('js/custom.min.js') }}"></script>
+<!--Style Switcher -->
+<script src="{{ URL::asset('plugins/bower_components/styleswitcher/jQuery.style.switcher.js') }}"></script>
